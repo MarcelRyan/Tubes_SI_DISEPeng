80c39bf27d109fa4168c23ec25aff51f32c451f7
marcelryan <88817627+MarcelRyan@users.noreply.github.com>
2023-11-12 18:16:39 +0700

    feat: Setup odoo

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "DISEPeng Odoo",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Kelompok muSIngin",
+    'category': 'Uncategorized',
+    'description': """Module Odoo untuk perusahaan BPOM Makassar""",
+    'data': [
+        'views/template.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ]
+}
