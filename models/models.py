a36616a698acb979d06c14ec52b971fa3073a016
MarcelRyan <marcel.ryan2004@gmail.com>
2023-11-18 18:32:25 +0700

    fix: remove diskusi and percakapan model

--- models/models.py
+++ models/models.py
@@ -11,16 +11,7 @@ class Pegawai(models.Model):
     isAdmin = fields.Boolean(string='Admin', required=True, default=False)
     
     pengaduan_id = fields.Many2many('disepeng.pengaduan', string="Penanggung Jawab Kasus", required=True)
-    percakapan_ids = fields.One2many('disepeng.percakapan', 'pegawai_id', string="Pengirim Pesan")
     
-class Diskusi(models.Model):
-    _name = 'disepeng.diskusi'
-    _description = 'Data Diskusi Kasus Pengaduan Terkait'
-    
-    judul = fields.Char(string="Judul Forum", required=True)
-    
-    pengaduan_id = fields.Many2one('disepeng.pengaduan', string="Pengaduan ID", required=True, ondelete='cascade', unique=True, auto_join=True)
-    percakapan_ids = fields.One2many('disepeng.percakapan', 'diskusi_id', string="Percakapan Terkait")
 
 class Pengaduan(models.Model):
     _name = 'disepeng.pengaduan'
@@ -36,14 +27,3 @@ class Pengaduan(models.Model):
     emailPelapor = fields.Char(string="Email Pelapor", required=True)
     nomorPelapor = fields.Char(string="Nomor Pelapor", required=True)
     
-    diskusi_ids = fields.One2many('disepeng.diskusi', 'pengaduan_id', string="Diskusi Terkait")
-
-class Percakapan(models.Model):
-    _name = 'disepeng.percakapan'
-    _description = 'Data percakapan pada forum diskusi'
-    
-    pesan = fields.Text(string="Pesan", required=True)
-    waktu = fields.Datetime(string="Waktu Pengiriman", required=True)    
-
-    diskusi_id = fields.Many2one('disepeng.diskusi', string="Diskusi Terkait", required=True, ondelete='cascade')
-    pegawai_id = fields.Many2one('disepeng.pegawai', string="Pengirim pesan", required=True)
